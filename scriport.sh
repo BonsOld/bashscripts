45f368f20edbf15ed1bc10dbb01065d8e79d0a31
BonsOld <bensonyoung123@gmail.com>
2020-11-02 15:47:03 -0700

    modified sciport to remove the need for user input

--- scriport.sh
+++ scriport.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 # Exports scripts from the current directory to ~/.local/bin and removes the .sh from the name
-echo "Which script is being exported?"
-read file
-cp $file ~/.local/bin
-mv ~/.local/bin/$file ~/.local/bin/`ls $file | awk -F. '{print $1}'`
+cp $1 ~/.local/bin
+mv ~/.local/bin/$1 ~/.local/bin/`ls $1 | awk -F. '{print $1}'`
